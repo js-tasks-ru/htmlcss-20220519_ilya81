38c59ac7159b435472ad7faa2fb8adb81b483f2e
Ilya Voloskov <ilya@2lemetry.ru>
2022-06-19 12:17:32 +0300

    modal

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,18 +2,28 @@
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
+    const modalSubmit = document.getElementById('modal-submit');
+    const shadow = document.getElementById('shadow');
 
-    if (!close || !modalOpen || !modal) {
+    if (!close || !modalOpen || !modal || !modalSubmit || !shadow) {
         return;
     }
 
     modalOpen.addEventListener('click', () => {
         modal.style.display = 'block';
+        shadow.style.display = 'block';
         document.body.overflow = 'hidden';
     });
 
     close.addEventListener('click', () => {
         modal.style.display = 'none';
+        shadow.style.display = 'none';
+        document.body.overflow = 'initial';
+    });
+
+    modalSubmit.addEventListener('click', () => {
+        modal.style.display = 'none';
+        shadow.style.display = 'none';
         document.body.overflow = 'initial';
     });
 })();
