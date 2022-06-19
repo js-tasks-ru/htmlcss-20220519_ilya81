ae9aaaf83f5c3d300fa3b6553c1fe2491b993eaf
Ilya Voloskov <ilya@2lemetry.ru>
2022-06-19 14:40:57 +0300

    modal fix

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -3,27 +3,23 @@
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
     const modalSubmit = document.getElementById('modal-submit');
-    const shadow = document.getElementById('shadow');
 
-    if (!close || !modalOpen || !modal || !modalSubmit || !shadow) {
+    if (!close || !modalOpen || !modal || !modalSubmit) {
         return;
     }
 
     modalOpen.addEventListener('click', () => {
         modal.style.display = 'block';
-        shadow.style.display = 'block';
         document.body.overflow = 'hidden';
     });
 
     close.addEventListener('click', () => {
         modal.style.display = 'none';
-        shadow.style.display = 'none';
         document.body.overflow = 'initial';
     });
 
     modalSubmit.addEventListener('click', () => {
         modal.style.display = 'none';
-        shadow.style.display = 'none';
         document.body.overflow = 'initial';
     });
 })();
